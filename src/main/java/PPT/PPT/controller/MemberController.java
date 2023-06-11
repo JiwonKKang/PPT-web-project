d25c7b1470814e0087e4645340006a6bf1d32b29
강지원 <kangji149@naver.com>
2023-06-11 19:42:24 +0900

    members

--- src/main/java/PPT/PPT/controller/MemberController.java
+++ src/main/java/PPT/PPT/controller/MemberController.java
@@ -14,8 +14,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @Slf4j
 @CrossOrigin(origins = "*")
@@ -23,7 +24,6 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    @ResponseBody
     @PostMapping("/members/new")
     public ResponseEntity<MemberResponseDto> createMember(@RequestBody MemberRequestDto dto) {
         Member member = new Member(dto.getName(),
@@ -36,10 +36,13 @@ public class MemberController {
     }
 
     @GetMapping("/members")
-    public String getMembers(@ModelAttribute("memberSearch") MemberSearch cond, Model model) {
+    public ResponseEntity<List<MemberResponseDto>> getMembers(@RequestBody MemberSearch cond) {
         List<Member> members = memberService.findMembers(cond);
-        model.addAttribute("members", members);
-        return "members/memberList";
+        List<MemberResponseDto> dtos = members
+                .stream()
+                .map(MemberResponseDto::from)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok().body(dtos);
     }
 
 //    @ResponseBody
